85d23d6b102370aedaa27ef603c3cef0c906b97f
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-01-17 11:09:08 +0100

    Changement de la police de l'heure

--- topbar.cpp
+++ topbar.cpp
@@ -15,6 +15,11 @@ TopBar::TopBar(const QSize &size) : QGraphicsPixmapItem(), _size(size) {
 void TopBar::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
     QGraphicsPixmapItem::paint(painter, option, widget);
     painter->setPen(Settings::foregroundColor());
+    QFont f;
+    f.setStyleHint(QFont::Helvetica);
+    f.setPointSize(15);
+    f.setWeight(QFont::DemiBold);
+    painter->setFont(f);
     unsigned int w = size().width() / 15;
     painter->drawText(size().width() - w, 0, w, size().height(), Qt::AlignCenter | Qt::TextWordWrap, QTime::currentTime().toString("hh:mm"));
 }
