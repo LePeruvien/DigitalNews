ec1db6ba981e549740312c9a80f7cd8e95407a7a
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-01-18 22:30:11 +0100

    Mise en place d'une boucle principale

--- rss/src/main.cpp
+++ rss/src/main.cpp
@@ -7,62 +7,49 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	string feed_url;
-	map<string, string> articles;
+	std::map<std::string, FeedReader::Entry> articles;
 
 	// validate arguments
-	if (argc == 2) 
-	{
+	if (argc == 2)  {
 		feed_url = argv[1];
-	}
-	else
-	{
-		cerr << "usage: FeedReaderTester.exe <feed_url>\n";
-		return 1;
+	} else {
+		feed_url = "http://www.ladepeche.fr/rss/39.rss";
 	}
 
-	try
-	{
+	try {
 		// init once per process
 		FeedReader::Feed::Initialize();
 
 		// create custom feed config with the path of the xsl files
 		FeedReader::FeedConfig feedConfig("./xsl");
 
-		// optionally, customize the list of xsl files and feed types 
-		// feedConfig.m_feed_types.clear();
-		// feedConfig.m_feed_types.push_back(std::make_pair("BroadCatching_02_xsl.txt", "BroadCatching02"));
-
 		// create feed reader
 		FeedReader::Feed feedReader(feed_url,feedConfig);
 
-		// check feed
-		feedReader.CheckFeed();
+		while (true) {
+			// check feed
+			feedReader.CheckFeed();
 
-		// get results
-		std::cout << "----------Feed---------" <<endl
-			<< "URL: '" << feedReader.GetUrl() << "'" << endl;
-		// print channel elements
-		for (FeedReader::Feed::feed_element_iterator fitr = feedReader.begin_feed_elements();
-				fitr != feedReader.end_feed_elements(); fitr++)
-		{
-			// std::cout << fitr->first << ":	'" <<  fitr->second << "'" << endl;
-		}
+			// get results
+			std::cout << "----------Feed---------" << std::endl << "URL: '" << feedReader.GetUrl() << "'" << std::endl;
 
-		// print item elements
-		for (FeedReader::Feed::entry_iterator itr = feedReader.begin_entries();
-				itr != feedReader.end_entries(); itr++)
-		{
-			std::cout << "afficher un nouvel article" << std::endl;
-			// std::cout << "Item ID: '"	<< itr->UniqueId	<< "'" << std::endl
-			// << "IsLive: '"	<< itr->IsLive		<< "'" << std::endl;
-			//itr->Print(std::cout);
-			for (FeedReader::Entry::entry_element_iterator itr2 = itr->begin_entry_elements(); itr2 != itr->end_entry_elements(); ++itr2) 		{
-				//articles[itr2->first] = itr2->second;
+			// print item elements
+			for (FeedReader::Feed::entry_iterator itr = feedReader.begin_entries(); itr != feedReader.end_entries(); itr++) {
+				std::cout << "UID : " << itr->UniqueId << std::endl;
+				if (articles.find(itr->UniqueId) == articles.end()) {
+					articles.insert(std::pair<std::string, FeedReader::Entry>(itr->UniqueId, *itr));
+					//TODO: send on ivy
+					std::cout << "Ajouté !" << std::endl;
+				} else {
+					std::cout << "Déjà présent." << std::endl;
+				}
 			}
-			for(map<string, string>::iterator it=articles.begin(); it!=articles.end(); ++it)
-			{
-				cout << it->first << " --- " << it->second << " --- " << endl;
-			}
+			sleep(60);
+
+
+			/*for (FeedReader::Entry::entry_element_iterator itr2 = itr->begin_entry_elements(); itr2 != itr->end_entry_elements(); ++itr2) 		{
+			articles[itr2->first] = itr2->second;
+			}*/
 		}
 	}
 	catch (exception& e)
