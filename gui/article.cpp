809a6ef62f2f056e1f4bf57d5e8faeef37252ffa
Buffon Nicolas <jokahero@gmail.com>
2012-01-18 11:45:43 +0100

    Modification des méthode d'affichage: mise en page.

--- gui/article.cpp
+++ gui/article.cpp
@@ -10,15 +10,15 @@ void Article::setContent(const QString &title, const QString &text, Topic topic)
 	_text = text;
 
 	if (topic == News)
-		_icon = new QPixmap(":/images/icons/newsIcon");
+		_icon = new QPixmap(":/icons/newsIcon");
 	else if (topic == Sport)
-		_icon = new QPixmap(":/images/icons/sportIcon");
+		_icon = new QPixmap(":/icons/sportIcon");
 	else if (topic == Adverts)
-		_icon = new QPixmap(":/images/icons/advertsIcon");
+		_icon = new QPixmap(":/icons/advertsIcon");
 	else if (topic == Economy)
 		_icon = new QPixmap(":/icons/economyIcon");
 	else if (topic == Culture)
-		_icon = new QPixmap(":/images/icons/cultureIcon");
+		_icon = new QPixmap(":/icons/cultureIcon");
 	else
 		_icon = NULL;
 }
@@ -31,15 +31,17 @@ QPixmap Article::draw(const QRect &size) const {
     QPixmap res(size.width(), size.height());
     res.fill(Qt::transparent);
 
-	QRect titleAdjustedRect = size.adjusted(10, 25, size.width(), size.height());
+	QRect titleAdjustedRect = QRect(10, 25, size.width() - 120, 60);
+	QRect contentAdjustedRect = size.adjusted(10, titleAdjustedRect.height() + 10, 0, 0);
+
 	int pixmapX = size.width() - 60;
 	int pixmapY = 10;
 
     QPainter draw(&res);
     draw.setPen(Qt::black);
 	draw.drawPixmap(pixmapX, pixmapY, 50, 50, *_icon);
-	draw.drawText(titleAdjustedRect, Qt::AlignTop | Qt::AlignLeft | Qt::TextWordWrap, _title);
-    draw.drawText(size, Qt::AlignCenter | Qt::TextWordWrap, _text);
+	draw.drawText(titleAdjustedRect, Qt::TextWordWrap, _title);
+	draw.drawText(contentAdjustedRect, Qt::TextWordWrap, _text);
     draw.end();
     return res;
 }
@@ -48,15 +50,17 @@ QPixmap Article::drawPreview(const QRect &size) const {
 	QPixmap res(size.width(), size.height());
 	res.fill(Qt::transparent);
 
-	QRect titleAdjustedRect = size.adjusted(10, 25, size.width(), size.height());
+	//QRect titleAdjustedRect = size.adjusted(10, 25, size.width() - 120 , 60);
+	QRect titleAdjustedRect = QRect(10, 25, size.width() - 120 , 60);
+	QRect contentAdjustedRect = size.adjusted(10, 85, 0, 0);
 	int pixmapX = size.width() - 60;
 	int pixmapY = 10;
 
 	QPainter draw(&res);
 	draw.setPen(Qt::black);
 	draw.drawPixmap(pixmapX, pixmapY, 50, 50, *_icon);
-	draw.drawText(titleAdjustedRect, Qt::AlignTop | Qt::AlignLeft | Qt::TextWordWrap, _title);
-	draw.drawText(size, Qt::AlignCenter | Qt::TextWordWrap, _text);
+	draw.drawText(titleAdjustedRect, Qt::TextWordWrap, _title);
+	draw.drawText(contentAdjustedRect, Qt::TextWordWrap, _text);
 	draw.end();
 	return res;
 }
